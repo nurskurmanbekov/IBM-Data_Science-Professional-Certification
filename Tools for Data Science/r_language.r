65a490ef4361d07ad5a0117c981fb7501e5379f1
Nursultan Kurmanbekov <nursultan.kurmanbekov@alatoo.edu.kg>
2023-02-25 17:22:58 +0600

    update

--- Tools for Data Science/r_language.r
+++ Tools for Data Science/r_language.r
@@ -1,2 +1,6 @@
-cars <- c(1,4,6,5,10)
-plot(cars)
+#Define the cars  vector  with 5 values
+cars <- c(1,4,5,6,7,8,10)
+#Graph the cars  vector  with all defaults 
+plot(cars, type='o')
+# Create title 
+title(main = "Cars vs Index")
