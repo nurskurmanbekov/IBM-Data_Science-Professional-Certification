b551e03da676ec7d7b90f11ad7faa52e5d2aa62d
Nursultan Kurmanbekov <nursultan.kurmanbekov@alatoo.edu.kg>
2023-02-26 11:42:48 +0600

    R experiment Result Plotting

--- Tools for Data Science/r_language.r
+++ Tools for Data Science/r_language.r
@@ -1,5 +1,7 @@
-library(datasets)
-# Load Data
-data(mtcars)
-# View first 5 rows
-head(mtcars, 5)
+#load ggplot package
+library(ggplot2)
+# create a scatterplot of displacement (disp) and miles per gallon (mpg)
+#load ggplot package
+library(ggplot2)
+# create a scatterplot of displacement (disp) and miles per gallon (mpg)
+ggplot(aes(x=disp,y=mpg,),data=mtcars)+geom_point()
